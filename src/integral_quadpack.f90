2658d5108704ed8c6132946a503775fc813d4409
jchristopherson <jchristopherson@hotmail.com>
2018-05-30 05:40:52 -0500

    Added quadpack error handler routine

--- src/integral_quadpack.f90
+++ src/integral_quadpack.f90
@@ -1,6 +1,8 @@
 ! integral_quadpack.f90
 
-! REF: http://www.netlib.org/quadpack/
+! REF:
+! - http://www.netlib.org/quadpack/
+! - https://people.sc.fsu.edu/~jburkardt/f_src/quadpack/quadpack.html
 module integral_quadpack
     use, intrinsic :: iso_fortran_env, only : int32, real64
     use integral_core
